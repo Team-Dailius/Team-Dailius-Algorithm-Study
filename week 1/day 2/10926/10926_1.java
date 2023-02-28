513967ec9e721a67a160c19a34b38356c4ecec21
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-03-01 02:23:54 +0900

    feat: 10926

---
+++ week 1/day 2/10926/10926_1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        
+        String input = stdIn.nextLine();
+        
+        System.out.println(input + "??!");
+    }
+}
