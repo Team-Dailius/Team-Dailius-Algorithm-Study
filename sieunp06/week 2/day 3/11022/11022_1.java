b8f10a59306a1a4b20188c66cc9716319b3544b2
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-03-08 23:15:13 +0900

    feat: 11022

---
+++ sieunp06/week 2/day 3/11022/11022_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        
+        int T = stdIn.nextInt();
+        
+        for (int i = 1; i <= T; i++) {
+            int A = stdIn.nextInt();
+            int B = stdIn.nextInt();
+            
+            System.out.println("Case #" + i + ": " + A + " + " + B + " = " + (A + B));
+        }
+    }
+}
